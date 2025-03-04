cd3f05d1d4c0c12bfd68ff60d508640f90aae3da
Hafiza Amna Razzaq <hafizaamnarazzaq@mac.home>
2025-03-03 23:30:27 -0500

    StarBackground fixed

--- components/main/StarBackground.tsx
+++ components/main/StarBackground.tsx
@@ -4,7 +4,7 @@ import React, { useState, useRef, Suspense } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
 import { Points, PointMaterial, Preload } from "@react-three/drei";
 import * as THREE from "three";
-// @ts-ignore
+// @ts-expect-error
 import * as random from "maath/random/dist/maath-random.esm";
 
 const StarBackground = (props: any) => {
